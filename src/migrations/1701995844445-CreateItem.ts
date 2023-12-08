c9f6c7c11d7689f29b5b32d8508b64fd2f9b599a
ISAWASHUN <syun20000616@icloud.com>
2023-12-08 21:08:58 +0900

    databaseの実装の完了

---
+++ src/migrations/1701995844445-CreateItem.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateItem1701995844445 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
